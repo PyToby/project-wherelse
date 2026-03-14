d48d61cce9b0ebf25e082ce89a4ff48bf13a471c
Tobiáš Radkovský <toby.radkovsky@gmail.com>
2026-03-14 13:02:31 +0100

    changes

--- website/__init__.py
+++ website/__init__.py
@@ -3,7 +3,7 @@ import os
 
 
 def create_app(): 
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='templates')
 
     from .views import view
     app.register_blueprint(view, url_prefix='/')
