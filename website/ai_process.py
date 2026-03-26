f80ae8869011a371e2145801ac1b7164dd679def
Tobiáš Radkovský <toby.radkovsky@gmail.com>
2026-03-26 17:24:42 +0100

    remove

--- website/ai_process.py
+++ website/ai_process.py
@@ -2,8 +2,8 @@ import google.genai as genai
 import os
 
 def generate_response(a, b):
-    #client = genai.Client(api_key=os.getenv("API_KEY"))
-    client = genai.Client(api_key='AIzaSyCIRWDmpm5Jxy5_ZX3IL5u9UOHKJr753T8')
+    client = genai.Client(api_key=os.getenv("API_KEY"))
+    
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=f'Compare these two services: {a} and {b}. Summarize their differences into ONE sentence only. Return only json formated text in this format: {{ "comparison": "data"}}' 
